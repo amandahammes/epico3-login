1a28f02160b0ddd7d3169437c05c06f6b83cb675
Amanda Hammes <amandahammes@gmail.com>
2024-10-10 12:18:38 -0300

    feat: ajustando css do forms e colocando interacao na tela com js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,26 @@
+const inputEmail = document.getElementById('email');
+const inputPassword = document.getElementById('password');
+
+inputEmail.addEventListener('focus', function() {
+    this.placeholder = '';  
+});
+
+inputPassword.addEventListener('focus', function() {
+    this.placeholder = '';  
+});
+
+inputEmail.addEventListener('blur', function() {
+    this.placeholder = 'Digite seu nomexemplo@gmail.com';
+});
+
+inputPassword.addEventListener('blur', function() {
+    this.placeholder = '0123456';
+});
+
+document.querySelector('.google-login-btn').addEventListener('click', function() {
+    window.location.href = 'https://www.google.com'; 
+});
+
 function esconderElemento() {
     var larguraTela = window.innerWidth;
     var elemento = document.getElementById("espaco-logo");
